2e02b7f169fc81a2f962956e2f97089f3da1b8b5
MichailI <mishika1221@mail.ru>
2021-10-04 23:22:21 +0500

    final step

--- src/index.js
+++ src/index.js
@@ -2,20 +2,28 @@ import "/styles/index.scss";
 import { sendRequest } from "/scripts/request";
 import { Card } from "/scripts/Card";
 
-let cards = []; // array of cards
-let i = 0; // iterator
+
+let cards = "";
+const $field = document.querySelector(".example-field");
+const $inputF = document.querySelector('.filter__input');
 
 sendRequest()
   .then((data) => {
-    console.log(data);
-    for (const key in data) {
-      const element = data[key];
-      let card = new Card(element);
-      cards[i] = card;
-      i++;
-    }
-    console.log('cards: ', cards);
+    createElements(data);
   })
   .catch((err) => {
     console.log(`%c${err}`, "color: red");
   });
+
+let createElements = (data) => {
+    for (const key in data) {
+        const element = data[key];
+        let card = new Card(element);
+        cards += card.strCard;
+      }
+      $field.innerHTML = cards;
+};
+
+$inputF.addEventListener('change', () => {
+    console.log(this.value);
+});
--- src/scripts/Card.js
+++ src/scripts/Card.js
@@ -1,13 +1,20 @@
-const $field = document.querySelector(".example-field");
-
 export class Card {
+  strCard;
+
   constructor(element) {
-    let card = `<div class="card">
-    <div class="card__img"></div>
-    <div>${element?.title}</div>
-    <div>${element?.address}</div>
-    <div>New Properties for Sale from &#163${element?.price}</div>
-    <div>${element?.type}</div>
+    this.strCard = `<div class="card card${this.color(element?.type)}">
+    <div class="card__img img${element?.id}" id="${element?.id}"></div>
+    <div class="card__title">${element?.title}</div>
+    <div class="card__address">${element?.address}</div>
+    <div class="card__cost">New Properties for Sale from <span class="bold">&#163 ${
+      element?.price
+    }</span></div>
+    <div class="card__type">${element?.type}</div>
     </div>`;
+  }
+
+  color(type) {
+    if (type === "IndependentLiving") return `-blue`;
+    else return `-orange`;
   }
 }
